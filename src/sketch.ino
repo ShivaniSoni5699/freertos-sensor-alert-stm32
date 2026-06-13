a10a42173cebea0185992785f5ff599f65867ff7
shivani555 <75723356+shivani555@users.noreply.github.com>
2026-06-13 10:57:56 +0200

    added watchdog

--- src/sketch.ino
+++ src/sketch.ino
@@ -2,7 +2,7 @@
 
 QueueHandle_t xQueue;
 SemaphoreHandle_t xSemaphore;
-
+SemaphoreHandle_t xWatchdogSemaphore;
 //Task 1
 void vTaskSensor(void *pvParameters)
 {
@@ -12,6 +12,7 @@ void vTaskSensor(void *pvParameters)
     {
       Sensorvalue = analogRead(A0);
       xQueueSend(xQueue, &Sensorvalue, portMAX_DELAY);
+      xSemaphoreGive(xWatchdogSemaphore);
       vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
@@ -24,6 +25,10 @@ void vTaskProcess(void *pvParameters)
   for(;;)
   {
     if(xQueueReceive(xQueue, & received, portMAX_DELAY)){
+      Serial.print(millis());
+      Serial.print(",");
+      Serial.print(received);
+      Serial.print(",");
       if (received > 511)
       {
         Serial.println("Alert:value too high");
@@ -53,16 +58,36 @@ void vTaskDisplay(void * pvParameters)
     }
   }
 }
+//task 4
+void vTaskWatchdog(void *pvParameters)
+{
+  for(;;)
+  {
+    if(xSemaphoreTake(xWatchdogSemaphore,pdMS_TO_TICKS(3000)) == pdFALSE)
+    {
+      Serial.println("WATCHDOG,0,FAULT");
+    }
+
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   xQueue = xQueueCreate(5,sizeof(uint32_t));
   xSemaphore = xSemaphoreCreateBinary();
+  xWatchdogSemaphore = xSemaphoreCreateBinary();
   xTaskCreate(vTaskSensor, "Sensor", 128, NULL ,1, NULL);
   xTaskCreate(vTaskProcess, "Process",128, NULL, 2, NULL);
   xTaskCreate(vTaskDisplay, "Display",128, NULL, 1, NULL);
+  xTaskCreate(vTaskWatchdog, "Watchdog", 128, NULL,3, NULL);
   vTaskStartScheduler();
+  
 }
 void loop() {
-
+//empty -- freertos takes charge 
 }
+
+
+// task1 -- queue(data) -- task2
+// task2 -- semaphore -- task3
+// task1 -- watchdog -- task4 
