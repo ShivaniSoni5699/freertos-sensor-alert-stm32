a7fadbbe995effe58208260b297df9ea4c1f5cc8
shivani555 <75723356+shivani555@users.noreply.github.com>
2026-06-16 07:05:30 +0200

    additional task to read temperature

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,8 +1,11 @@
 #include <STM32FreeRTOS.h>
-
+//#include <DHT.h>
+//32Kb flash
 QueueHandle_t xQueue;
 SemaphoreHandle_t xSemaphore;
 SemaphoreHandle_t xWatchdogSemaphore;
+//DHT dht(4,DHT22);
+
 //Task 1
 void vTaskSensor(void *pvParameters)
 {
@@ -58,7 +61,23 @@ void vTaskDisplay(void * pvParameters)
     }
   }
 }
-//task 4
+//task4 
+void vTaskTemp(void *pvParameters)
+{
+  vTaskDelay(pdMS_TO_TICKS(500));  // wait 500ms before first reading
+  for(;;)
+  {
+    float temp = map(analogRead(A1), 0, 1023, 0, 50);
+    //float temp = dht.readTemperature();
+    Serial.print(millis());
+    Serial.print(",");
+    Serial.print(temp);
+    Serial.print(",");
+    Serial.println("TEMP");
+    vTaskDelay(pdMS_TO_TICKS(2000));
+  }
+}
+//task 5
 void vTaskWatchdog(void *pvParameters)
 {
   for(;;)
@@ -71,6 +90,7 @@ void vTaskWatchdog(void *pvParameters)
   }
 }
 
+
 void setup() {
   Serial.begin(115200);
   xQueue = xQueueCreate(5,sizeof(uint32_t));
@@ -80,6 +100,8 @@ void setup() {
   xTaskCreate(vTaskProcess, "Process",128, NULL, 2, NULL);
   xTaskCreate(vTaskDisplay, "Display",128, NULL, 1, NULL);
   xTaskCreate(vTaskWatchdog, "Watchdog", 128, NULL,3, NULL);
+  xTaskCreate(vTaskTemp, "Temp", 128, NULL, 1 , NULL);
+  //dht.begin();
   vTaskStartScheduler();
   
 }
